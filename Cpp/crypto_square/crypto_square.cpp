51dc595ad948e49938116ce9ea11f674138b4ad6
Lignite17 <116222565+Lignite17@users.noreply.github.com>
2022-10-29 13:56:25 -0700

    Add Crypto Square solution

--- Cpp/crypto_square/crypto_square.cpp
+++ Cpp/crypto_square/crypto_square.cpp
@@ -2,6 +2,88 @@
 
 namespace crypto_square {
 
+cipher::cipher(const std::string& input) {
+    m_normalizedString = cleanString(input);
+    m_normalizedLength = m_normalizedString.size();
+    m_inputSegments = createSegments(m_normalizedString);
+    m_cleanOutputString = readSegments(m_inputSegments);
+}
 
+std::string cipher::cleanString(const std::string& input) {
+    std::string output{ input };
+    std::size_t itr{ 0 };
+    
+    while (itr < output.size()) {
+        if (output[itr] >= 'A' && output[itr] <= 'Z') {
+            std::string newLetter{ std::string(1,('a' + (output[itr] - 'A'))) };
+            output.replace(itr, 1, newLetter);
+            itr++;
+        }
+        else if ((output[itr] < 'a' || output[itr] > 'z') && (output[itr] < '0' || output[itr] > '9')) {
+            output.erase(itr,1);
+        }
+        else {
+            itr++;
+        }
+    }
+
+    return output;
+}
+
+std::vector<std::string> cipher::createSegments(const std::string& inputString) {
+    std::vector<std::string> outputVector;
+    unsigned int charLength{ 0 };
+
+    if (m_normalizedLength > 0) {
+
+        while (!(m_normalizedLength < (charLength * charLength) - charLength)) {
+            ++charLength;
+        }
+
+        if ((charLength-1) * (charLength-1) == m_normalizedLength) {
+            --charLength;
+        }
+
+        unsigned int itr{ 0 };
+
+        while (itr*charLength < m_normalizedLength) {
+            std::string thisSegment{ "" };
+
+            for (unsigned int j = 0; j < charLength; ++j) {
+                if ((j+(itr*charLength)) < m_normalizedLength) {
+                    thisSegment += inputString[j+(itr*charLength)];
+                }
+            }
+
+            itr++;
+            outputVector.push_back(thisSegment);
+        }
+    }
+
+    m_segmentLength = charLength;
+    return outputVector;
+}
+
+std::string cipher::readSegments(const std::vector<std::string>& segments) {
+    std::string outputString;
+
+    for (int colNum = 0; colNum < m_segmentLength; ++colNum) {
+        for (std::size_t rowNum = 0; rowNum < segments.size(); ++rowNum) {
+            if (((segments[rowNum]))[colNum] != '\000') {
+                outputString += std::string(1,(segments[rowNum])[colNum]);
+                m_normalizedOutputString += std::string(1,(segments[rowNum])[colNum]);
+            }
+            else {
+                m_normalizedOutputString += " ";
+            }
+        }
+        
+        if ((colNum+1) != m_segmentLength) {
+            m_normalizedOutputString += " ";
+        }
+    }
+
+    return outputString;
+}
 
 }
--- Cpp/crypto_square/crypto_square.h
+++ Cpp/crypto_square/crypto_square.h
@@ -7,13 +7,25 @@
 namespace crypto_square {
 
 class cipher {
-public:
-    cipher(std::string input);
+private:
+    std::string m_normalizedString;
+    std::size_t m_normalizedLength;
+    int m_segmentLength;
+    std::string m_normalizedOutputString;
+    std::string m_cleanOutputString;
+    std::vector<std::string> m_inputSegments;
 
-    std::string normalize_plain_text();
-    std::vector<std::string> plain_text_segments();
-    std::string cipher_text();
-    std::string normalized_cipher_text();
+    std::string cleanString(const std::string& input);
+    std::vector<std::string> createSegments(const std::string& inputString);
+    std::string readSegments(const std::vector<std::string>& segments);
+
+public:
+    cipher(const std::string& input);
+
+    std::string normalize_plain_text() const { return m_normalizedString; }
+    std::vector<std::string> plain_text_segments() { return m_inputSegments; }
+    std::string cipher_text() { return m_cleanOutputString; }
+    std::string normalized_cipher_text() { return m_normalizedOutputString; }
 
 };
 
