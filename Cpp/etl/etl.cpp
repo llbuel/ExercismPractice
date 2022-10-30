9178b5959155001c3b99cfc35df37dab6c808ff7
Lignite17 <116222565+Lignite17@users.noreply.github.com>
2022-10-29 17:37:55 -0700

    Add ETL exercise solution

--- Cpp/etl/etl.cpp
+++ Cpp/etl/etl.cpp
@@ -2,6 +2,24 @@
 
 namespace etl {
 
+std::map<char, int> transform(const std::map<int, std::vector<char>>& oldMap) {
+    std::map<char, int> newMap;
+    
+    for (std::map<int, std::vector<char>>::const_iterator mapItr = oldMap.begin(); mapItr != oldMap.end(); ++mapItr) {
+        for (std::vector<char>::const_iterator vecItr = mapItr->second.begin(); vecItr != mapItr->second.end(); ++vecItr) {
+            if (*vecItr >= 'A' && *vecItr <= 'Z') {
+                char newLetter = 'a' + (*vecItr - 'A');
+                std::pair<char, int> pairInsert = std::make_pair(newLetter, mapItr->first);
+                newMap.insert(pairInsert);
+            }
+            else if (*vecItr >= 'a' && *vecItr <= 'z') {
+                std::pair<char, int> pairInsert = std::make_pair(*vecItr, mapItr->first);
+                newMap.insert(pairInsert);
+            }
+        }
+    }
 
+    return newMap;
+}
 
 }
--- Cpp/etl/etl.h
+++ Cpp/etl/etl.h
@@ -1,9 +1,12 @@
 #ifndef ETL_H
 #define ETL_H
 
+#include <map>
+#include <vector>
+
 namespace etl {
 
-
+std::map<char, int> transform(const std::map<int, std::vector<char>>& oldMap);
 
 }
 
