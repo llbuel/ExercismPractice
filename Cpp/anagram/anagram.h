f5d14bcac35320d03949b0bc4f724a35aac07939
Lignite17 <116222565+Lignite17@users.noreply.github.com>
2022-10-29 21:20:03 -0700

    Add Anagram exercise

--- Cpp/anagram/anagram.h
+++ Cpp/anagram/anagram.h
@@ -3,20 +3,7 @@
 
 namespace anagram {
 
-class templateClass{
-private:
-    int m_priv_variable;
 
-public:
-    int m_pub_variable;
-
-    templateClass(int priv, int pub);
-
-    int classFcn();
-    int classFcn2(int input1, int input2);
-};
-
-int localFcn(int input);
 
 }
 
