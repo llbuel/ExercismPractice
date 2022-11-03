933a1faeb373105c1b93b928d35d2f86e60296ec
Lignite17 <116222565+Lignite17@users.noreply.github.com>
2022-11-02 19:57:50 -0700

    Add Robot Simulator solution

--- Cpp/robot_simulator/robot_simulator.cpp
+++ Cpp/robot_simulator/robot_simulator.cpp
@@ -2,6 +2,78 @@
 
 namespace robot_simulator {
 
+const int Bearing::NORTH{100};
+const int Bearing::SOUTH{101};
+const int Bearing::EAST{110};
+const int Bearing::WEST{111};
 
+void Robot::turn_right() {
+    switch (m_currentBearing) {
+        case Bearing::NORTH:
+            m_currentBearing = Bearing::EAST;
+            break;
+        case Bearing::EAST:
+            m_currentBearing = Bearing::SOUTH;
+            break;
+        case Bearing::SOUTH:
+            m_currentBearing = Bearing::WEST;
+            break;
+        case Bearing::WEST:
+            m_currentBearing = Bearing::NORTH;
+            break;
+    }
+}
+
+void Robot::turn_left() {
+    switch (m_currentBearing) {
+        case Bearing::NORTH:
+            m_currentBearing = Bearing::WEST;
+            break;
+        case Bearing::WEST:
+            m_currentBearing = Bearing::SOUTH;
+            break;
+        case Bearing::SOUTH:
+            m_currentBearing = Bearing::EAST;
+            break;
+        case Bearing::EAST:
+            m_currentBearing = Bearing::NORTH;
+            break;
+    }
+}
+
+void Robot::advance() {
+    switch (m_currentBearing) {
+        case Bearing::NORTH:
+            m_currentPosition.second += 1;
+            break;
+        case Bearing::EAST:
+            m_currentPosition.first += 1;
+            break;
+        case Bearing::SOUTH:
+            m_currentPosition.second -= 1;
+            break;
+        case Bearing::WEST:
+            m_currentPosition.first -= 1;
+            break;
+    }
+}
+
+void Robot::execute_sequence(const std::string& movementSequence) {
+    for(std::size_t i = 0; i < movementSequence.size(); ++i) {
+        switch (movementSequence[i]) {
+            case 'R':
+                turn_right();
+                break;
+            case 'L':
+                turn_left();
+                break;
+            case 'A':
+                advance();
+                break;
+            default:
+                break;
+        }
+    }
+}
 
 }
--- Cpp/robot_simulator/robot_simulator.h
+++ Cpp/robot_simulator/robot_simulator.h
@@ -1,9 +1,45 @@
 #ifndef ROBOT_SIMULATOR_H
 #define ROBOT_SIMULATOR_H
 
+#include <utility>
+#include <string>
+
 namespace robot_simulator {
 
+struct Bearing {
+private:
+    int m_bearing{};
 
+public:
+    static const int NORTH;
+    static const int SOUTH;
+    static const int EAST;
+    static const int WEST;
+
+    Bearing(int bearing) { m_bearing = bearing; }
+
+    int getBearing() const { return m_bearing; }
+};
+
+class Robot {
+private:
+    std::pair<int, int> m_startPosition;
+    int m_startBearing;
+    std::pair<int, int> m_currentPosition;
+    int m_currentBearing;
+
+public:
+    Robot() : m_startPosition({0, 0}), m_startBearing(Bearing::NORTH), m_currentPosition(m_startPosition), m_currentBearing(m_startBearing) {}
+    Robot(std::pair<int, int> startPosition, Bearing startBearing) : m_startPosition(startPosition), m_startBearing(startBearing.getBearing()), m_currentPosition(m_startPosition), m_currentBearing(m_startBearing) {}
+
+    void turn_right();
+    void turn_left();
+    void advance();
+    void execute_sequence(const std::string& movementSequence);
+    std::pair<int, int> get_position() const { return m_currentPosition; }
+    int get_bearing() const { return m_currentBearing; }
+
+};
 
 }
 
