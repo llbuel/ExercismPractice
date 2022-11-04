5d3bdf19b3655b8b4ff88d3d8c89a91b2b546393
Lignite17 <116222565+Lignite17@users.noreply.github.com>
2022-11-04 11:39:57 -0700

    Add Scrabble Score solution

--- Cpp/scrabble_score/scrabble_score.cpp
+++ Cpp/scrabble_score/scrabble_score.cpp
@@ -2,6 +2,49 @@
 
 namespace scrabble_score {
 
+static const std::map<int, std::vector<char>> scoreMap = {{1,{'A','E','I','O','U','L','N','R','S','T'}},
+                                                          {2,{'D','G'}},
+                                                          {3,{'B','C','M','P'}},
+                                                          {4,{'F','H','V','W','Y'}},
+                                                          {5,{'K'}},
+                                                          {6,{}},
+                                                          {7,{}},
+                                                          {8,{'J','X'}},
+                                                          {9,{}},
+                                                          {10,{'Q','Z'}}};
 
+std::string normalizeString(const std::string& inputString) {
+    std::string outputString{ inputString };
+    
+    for (std::size_t i = 0; i < outputString.size(); ++i) {
+        if (outputString[i] >= 'a' && outputString[i] <= 'z') {
+            char newLetter{ static_cast<char>((outputString[i]-'a') + 'A') };
+            outputString[i] = newLetter;
+        }
+    }
+
+    return outputString;
+}
+
+int getLetterScore(char letter) {
+    for (std::map<int, std::vector<char>>::const_iterator mapItr = scoreMap.begin(); mapItr != scoreMap.end(); ++mapItr) {
+        if (std::find(mapItr->second.begin(), mapItr->second.end(), letter) != mapItr->second.end()) {
+            return mapItr->first;
+        }
+    }
+
+    return 0;
+}
+
+int score(const std::string& word) {
+    std::string cleanWord{ normalizeString(word) };
+    int scoreSum{ 0 };
+
+    for (std::size_t i = 0; i < cleanWord.size(); ++i) {
+        scoreSum += getLetterScore(cleanWord[i]);
+    }
+
+    return scoreSum;
+}
 
 }
--- Cpp/scrabble_score/scrabble_score.h
+++ Cpp/scrabble_score/scrabble_score.h
@@ -1,9 +1,14 @@
 #ifndef SCRABBLE_SCORE_H
 #define SCRABBLE_SCORE_H
 
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+
 namespace scrabble_score {
 
-
+int score(const std::string& word);
 
 }
 
