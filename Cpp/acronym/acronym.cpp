680f0456d4732e37607dcd1d0e8254615b5284e6
Lignite17 <116222565+Lignite17@users.noreply.github.com>
2022-11-03 19:59:37 -0700

    Add Acronym solution

--- Cpp/acronym/acronym.cpp
+++ Cpp/acronym/acronym.cpp
@@ -2,6 +2,39 @@
 
 namespace acronym {
 
+std::string cleanString(const std::string& inputString) {
+    std::string outputString{ inputString };
 
+    for (std::size_t i = 0; i < outputString.size(); ++i) {
+        if (outputString[i] >= 'a' && outputString[i] <= 'z') {
+            char newChar{ static_cast<char>((outputString[i]-'a') + 'A') };
+            outputString[i] = newChar;
+        }
+        else if ((outputString[i] < 'A' || outputString[i] > 'Z') && (outputString[i] < '0' || outputString[i] > '9')) {
+            outputString.replace(i,1," ");
+        }
+    }
+
+    return outputString;
+}
+
+std::string acronym(const std::string& fullPhrase) {
+    std::string cleanPhrase{ cleanString(fullPhrase) };
+    std::size_t itr{ 0 };
+    std::string outputAcronym;
+
+    while (itr != std::string::npos) {
+        if (itr == 0){
+            outputAcronym += cleanPhrase[0];
+        }
+        else if (itr != (cleanPhrase.size()-1) && cleanPhrase[itr+1] != ' ') {
+            outputAcronym += cleanPhrase[itr+1];
+        }
+
+        itr = cleanPhrase.find(' ',itr+1);
+    }
+
+    return outputAcronym;
+}
 
 }
--- Cpp/acronym/acronym.h
+++ Cpp/acronym/acronym.h
@@ -1,9 +1,11 @@
 #ifndef ACRONYM_H
 #define ACRONYM_H
 
+#include <string>
+
 namespace acronym {
 
-
+std::string acronym(const std::string& fullPhrase);
 
 }
 
