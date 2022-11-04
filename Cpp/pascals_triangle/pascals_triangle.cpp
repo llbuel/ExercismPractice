09a60e1baaeaf3591a8b6aaf886ee092df7725ef
Lignite17 <116222565+Lignite17@users.noreply.github.com>
2022-11-03 20:34:01 -0700

    Add Pascal's Triangle solution

--- Cpp/pascals_triangle/pascals_triangle.cpp
+++ Cpp/pascals_triangle/pascals_triangle.cpp
@@ -2,6 +2,32 @@
 
 namespace pascals_triangle {
 
+std::vector<std::vector<int>> generate_rows(int rowCount) {
+    std::vector<std::vector<int>> triangle;
 
+    if (rowCount == 1) {
+        triangle.push_back({1});
+    }
+    else if (rowCount == 2) {
+        triangle.push_back({1});
+        triangle.push_back({1,1});
+    }
+    else if (rowCount > 2) {
+        triangle = generate_rows(2);
+
+        for (int i = 2; i < rowCount; ++i) {
+            triangle.push_back({});
+            triangle[i].push_back(1);
+
+            for (int j = 0; j < (i-1); ++j) {
+                triangle[i].push_back(triangle[i-1][j]+triangle[i-1][j+1]);
+            }
+
+            triangle[i].push_back(1);
+        }
+    }
+
+    return triangle;
+}
 
 }
--- Cpp/pascals_triangle/pascals_triangle.h
+++ Cpp/pascals_triangle/pascals_triangle.h
@@ -1,9 +1,11 @@
 #ifndef PASCALS_TRIANGLE_H
 #define PASCALS_TRIANGLE_H
 
+#include <vector>
+
 namespace pascals_triangle {
 
-
+std::vector<std::vector<int>> generate_rows(int rowCount);
 
 }
 
