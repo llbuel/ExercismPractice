12c3aff1ee5dfb4e8da7c8020891380bfc5f3fa8
Lignite17 <116222565+Lignite17@users.noreply.github.com>
2022-11-03 21:05:16 -0700

    Add Armstrong Numbers solution

--- Cpp/armstrong_numbers/armstrong_numbers.cpp
+++ Cpp/armstrong_numbers/armstrong_numbers.cpp
@@ -2,6 +2,44 @@
 
 namespace armstrong_numbers {
 
+std::vector<int> getDigits(int inputNumber) {
+    std::vector<int> digits;
 
+    while (inputNumber > 0) {
+        digits.insert(digits.begin(),(inputNumber%10));
+        inputNumber /= 10;
+    }
+
+    return digits;
+}
+
+int power(int base, int exponent) {
+    if (base > 0 && exponent > 0) {
+        int result{ base };
+
+        for (int i = 1; i < exponent; ++i) {
+            result *= base;
+        }
+
+        return result;
+    }
+    else if (base > 0 && exponent == 0) {
+        return 1;
+    }
+
+    return 0;
+}
+
+bool is_armstrong_number(int testNumber) {
+    std::vector<int> digits{ getDigits(testNumber) };
+    std::size_t digitCount{ digits.size() };
+    int armSum{ 0 };
+
+    for (std::size_t i = 0; i < digitCount; ++i) {
+        armSum += power(digits[i], digitCount);
+    }
+
+    return (testNumber == armSum);
+}
 
 }
--- Cpp/armstrong_numbers/armstrong_numbers.h
+++ Cpp/armstrong_numbers/armstrong_numbers.h
@@ -1,8 +1,11 @@
 #ifndef ARMSTRONG_NUMBERS_H
 #define ARMSTRONG_NUMBERS_H
 
+#include <vector>
+
 namespace armstrong_numbers {
 
+bool is_armstrong_number(int testNumber);
 
 }
 
